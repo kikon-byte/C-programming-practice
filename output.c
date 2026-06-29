84043b4e19db769b062a87842694624bb40b0a0a
kikon-byte <obenrokikon@gmail.com>
2026-06-29 21:54:40 +0530

    OUTPUT FROM USER

---
+++ output.c
@@ -0,0 +1,13 @@
+output
+#include<stdio.h>
+int main() {
+    int x,y;
+    int sum;
+    printf("enter x =");
+    scandf("%d",&x);
+    printf("enter y =");
+    scanf("%d",&y);
+    sum=x+y;
+    printf("sum =%d",sum);
+    return 0;
+}
