a2f02d23d596a3e52101c7a04d04e827c3eef9ca
Obenro Kikon <obenrokikon@gmail.com>
2026-06-30 20:44:50 +0530

    structure of calculator code

--- simple arithmetic calculator in C.c
+++ simple arithmetic calculator in C.c
@@ -1,16 +1,20 @@
 simple arithmetic calculator in C 
-#include<stdio.h> int main() { int x,y; int sum, difference, product ,remainder;  float quotient ;
+#include<stdio.h> int main() { 
+     int x,y;
+     int sum, difference, product ,remainder; 
+     float quotient ;
      printf("enterx= ");
-      scanf("%d",&x);
-       printf("enter y= "); 
-       scanf("%d",&y); 
-       sum=x+y;  printf("sum=%d\n", sum);
-         difference=x-y;
-          printf("difference=%d\n", difference);
-           product=x*y;
-            printf("product=%d\n", product);
-             quotient=(float)x/y;
-              printf("quotient=%f\n", quotient); 
-    remainder=x%y; 
-    printf("remainder=%d\n",remainder); 
+     scanf("%d",&x);
+     printf("enter y= "); 
+     scanf("%d",&y); 
+     sum=x+y; 
+     printf("sum=%d\n", sum);
+     difference=x-y; 
+     printf("difference=%d\n", difference);
+     product=x*y;
+     printf("product=%d\n", product);
+     quotient=(float)x/y;
+     printf("quotient=%f\n", quotient); 
+     remainder=x%y; 
+     printf("remainder=%d\n",remainder); 
      return 0; }
